73ee4728004f6be2368a3fdf1feb6cc10328f281
ravi-gautam17 <180750145+ravi-gautam17@users.noreply.github.com>
2025-12-17 16:49:57 +0530

    [Ravi]: Conversion from kilometres to miles program added

---
+++ gcr-codebase/programming-elements/Level 1/ConvertFromKMtoMiles.java
@@ -0,0 +1,11 @@
+
+// this is the program to convert distance from kilometre to miles
+public class ConvertFromKMtoMiles {
+    public static void main(String[] args) {
+        double distanceInKM=10.8;
+        //calculate the distance in miles
+        double distanceInMiles=distanceInKM*0.621371;
+        System.out.println("The distance "+distanceInKM+"km in miles is "+distanceInMiles);
+
+    }
+}
