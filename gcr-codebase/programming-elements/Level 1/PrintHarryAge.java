c4df07785c0598fb17c7c8a0c2a74f822cc21f1e
ravi-gautam17 <180750145+ravi-gautam17@users.noreply.github.com>
2025-12-18 00:07:51 +0530

    [Ravi]: Print the harry's age program added

---
+++ gcr-codebase/programming-elements/Level 1/PrintHarryAge.java
@@ -0,0 +1,11 @@
+public class PrintHarryAge {
+    public static void main(String[] args) {
+        // we need to print the current age of harry
+        int yearOfBirth=2000;
+        int currentYear=2024;
+        //subtract the current year with year of birth
+        int age=currentYear-yearOfBirth;
+        //print the age
+        System.out.println("Harry's age in 2024 is "+age);
+    }
+}
