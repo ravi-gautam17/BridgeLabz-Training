d1cebccff4a806e310bcde8ff9fcb6183e66c76c
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2025-12-22 17:24:37 +0530

    [Ravi]: modified comments of arrays index out of bounds program

--- gcr-codebase/strings/Level 1/ShowArraysIndexOutOfBoundsExcept.java
+++ gcr-codebase/strings/Level 1/ShowArraysIndexOutOfBoundsExcept.java
@@ -5,17 +5,21 @@ import java.util.Scanner;
 public class ShowArraysIndexOutOfBoundsExcept {
 
     public static void main(String[] args) {
-
+        // Create a scanner object
         Scanner sc = new Scanner(System.in);
 		
+		// Read the size from the user
         System.out.print("Enter the size of array: ");		
 		int size=sc.nextInt();
+		
+		// Read the names
 		String[] names=new String[size];
 		
 		for(int i=0;i<names.length;i++){
 		   names[i]=sc.next();
 		}
-                
+            
+        //Generate and handle the exception			
 		try{
           String name=generateException(names);
 		}catch(ArrayIndexOutOfBoundsException e){
