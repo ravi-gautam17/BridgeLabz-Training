6999f8efb8fb09ce8c100bb36355740a5f5c91db
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2025-12-27 01:45:09 +0530

    [Ravi]: Display employee program added

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/classandobject/levelone/DisplayEmployee.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/classandobject/levelone/DisplayEmployee.java
@@ -2,7 +2,6 @@ package com.classandobject.levelone;
 
 public class DisplayEmployee {
 
-
 	public static void main(String[] args) {
 	Employee emp=new Employee("Rohan",1,500000);//calling constructor
 	emp.displayEmployee();//calling the display method
