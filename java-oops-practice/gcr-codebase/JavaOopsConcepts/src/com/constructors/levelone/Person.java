36ef66750e2d7ceabb1f6c453f856a90047d9bc4
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2025-12-27 17:01:12 +0530

    [Ravi]: Person program added

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/Person.java
@@ -0,0 +1,27 @@
+package com.constructors.levelone;
+
+public class Person {
+    private String name;
+    private int age;
+    Person(String name, int age){
+    	  this.name=name;
+    	  this.age=age;
+    }
+	Person(Person p){
+		this.name=p.name;
+		this.age=p.age;
+	}
+	public void show() {
+		System.out.println("Name: "+this.name);
+		System.out.println("Age: "+this.age);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Person p1=new Person("Ravi",22);
+        Person p2=new Person(p1);
+        p1.show();
+        p2.show();
+	}
+
+}
