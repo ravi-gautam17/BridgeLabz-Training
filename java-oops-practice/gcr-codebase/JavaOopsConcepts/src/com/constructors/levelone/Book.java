911866a5f2fc9d7a054284aee7ab47fb1aef3d52
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2025-12-27 16:25:56 +0530

    [Ravi]: Book program added

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/Book.java
@@ -0,0 +1,29 @@
+package com.constructors.levelone;
+
+public class Book {
+   private String title;
+   private  String author;
+   private double price;
+   Book(){
+	   System.out.println("Book constructor called");
+   };
+   Book(String title, String author,double price){
+       this.title=title;
+       this.author=author;
+       this.price=price;
+   }
+   public void show(){
+	   System.out.println("Title: "+this.title);
+	   System.out.println("Author: "+this.author);
+	   System.out.println("Price: "+this.price);
+	   
+   }
+   public static void main(String[] args) {
+	   Book obj1=new Book();
+	   Book obj2=new Book("Ramayana","Valmiki",1000.0);
+	   obj2.show();
+	   
+	   
+   }
+   
+}
