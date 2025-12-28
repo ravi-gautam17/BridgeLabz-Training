d36476ba801295d70d541691163b54cbe8ef790b
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2025-12-28 19:22:56 +0530

    [Ravi]: Post Graduate class added

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/PostgraduateStudent.java
@@ -0,0 +1,21 @@
+package com.constructors.levelone;
+
+public class PostgraduateStudent extends Student {
+
+    String specialization;
+
+    // Constructor
+    PostgraduateStudent(int rollNumber, String name, double cgpa, String specialization) {
+        super(rollNumber, name, cgpa);
+        this.specialization = specialization;
+    }
+
+    // Method demonstrating access to protected member
+    void displayPGStudentDetails() {
+        System.out.println("Roll Number     : " + rollNumber);  // public
+        System.out.println("Name            : " + name);        // protected
+        System.out.println("CGPA            : " + getCGPA());   // private via getter
+        System.out.println("Specialization  : " + specialization);
+    }
+}
+
