43976c263980c113f8daa52cc4834cf22389c4da
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-01 15:04:55 +0530

    [Ravi]: Book program added

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/libraryandbooks/Book.java
@@ -0,0 +1,19 @@
+package com.objectmodelling.libraryandbooks;
+
+
+public class Book {
+
+	private String title;
+	private String author;
+	private String isbn;
+	
+	public Book(String title, String author, String isbn) {
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+	}
+	
+	public void displayBook() {
+		System.out.println("Title: " + (title) + ", Author: " + (author) + ", ISBN: " + (isbn));
+	}
+}
