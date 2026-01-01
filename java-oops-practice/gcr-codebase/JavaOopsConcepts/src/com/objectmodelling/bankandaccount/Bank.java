42ca35cd10ef19ef9c1d92d817f7b0f8188da2cb
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-01 15:49:18 +0530

    [Ravi]: Bank and account program added

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/bankandaccount/Bank.java
@@ -0,0 +1,21 @@
+package com.objectmodelling.bankandaccount;
+
+public class Bank {
+    private String bankName;
+
+    public Bank(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    // Association method
+    public void openAccount(Customer customer, int accNo, double balance) {
+        Account account = new Account(accNo, balance, this);
+        customer.addAccount(account);
+
+        System.out.println("Account opened successfully at " + bankName);
+    }
+}
