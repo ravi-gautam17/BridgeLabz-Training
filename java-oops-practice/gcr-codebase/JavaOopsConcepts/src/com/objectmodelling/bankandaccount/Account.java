9c9b78b1d390e2934cb08a442bdeb2828741a70c
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-01 15:50:58 +0530

    [Ravi]: Bank and account program added

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/bankandaccount/Account.java
@@ -0,0 +1,26 @@
+package com.objectmodelling.bankandaccount;
+
+
+public class Account {
+    private int accountNumber;
+    private double balance;
+    private Bank bank;
+
+    public Account(int accountNumber, double balance, Bank bank) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.bank = bank;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
+}
