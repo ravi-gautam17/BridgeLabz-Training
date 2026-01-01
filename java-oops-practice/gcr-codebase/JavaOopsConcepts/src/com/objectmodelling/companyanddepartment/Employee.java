cb43296e286e595e8b6ccadef8a2e5141296b868
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-01 16:11:58 +0530

    [Ravi]: Employee program added

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/companyanddepartment/Employee.java
@@ -0,0 +1,16 @@
+package com.objectmodelling.companyanddepartment;
+
+public class Employee {
+    private int empId;
+    private String empName;
+
+    public Employee(int empId, String empName) {
+        this.empId = empId;
+        this.empName = empName;
+    }
+
+    public void displayEmployee() {
+        System.out.println("Employee ID: " + empId +
+                           ", Name: " + empName);
+    }
+}
