58343a2c15453214939baee042e5bfee02649916
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-02 16:31:17 +0530

    [Ravi]: Waiter program added

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/restaurantmanagement/Waiter.java
@@ -0,0 +1,12 @@
+package com.inheritance.hybridinheritance.restaurantmanagement;
+
+public class Waiter extends Person implements Worker{
+	
+	public Waiter(String name, int id) {
+		super(name, id);
+	}
+	@Override
+	public void performDuties() {
+		System.out.println("Waiter is serving foods");
+	}
+}
