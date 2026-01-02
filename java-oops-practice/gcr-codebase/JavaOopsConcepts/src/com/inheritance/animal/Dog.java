add31674dfe791749aca8a3bf16f3185ab7a9158
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-02 15:07:16 +0530

    [Ravi]: Dog program added

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/animal/Dog.java
@@ -0,0 +1,13 @@
+package com.inheritance.animal;
+
+public class Dog extends Animal{
+
+	Dog(String name, int age){
+		super(name, age);
+	}
+	
+	@Override
+	public void makeSound() {
+		System.out.println("Dog Barks");
+	}
+}
