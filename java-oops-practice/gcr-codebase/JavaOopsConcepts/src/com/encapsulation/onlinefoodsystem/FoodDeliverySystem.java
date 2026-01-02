1e7cc793224da3773aa500545c781a16f991e4a9
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-03 00:54:35 +0530

    [Ravi]: Food delivery system program added

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/onlinefoodsystem/FoodDeliverySystem.java
@@ -0,0 +1,35 @@
+package com.encapsulation.onlinefoodsystem;
+import java.util.*;
+
+public class FoodDeliverySystem {
+	public static void main(String[] args) {
+		List<FoodItem> cart = new ArrayList<>();
+        
+        VegItem paneer = new VegItem("Paneer Tikka", 12.0, 2);
+        paneer.applyDiscount(10); 
+        
+        cart.add(paneer);
+        cart.add(new NonVegItem("Chicken Burger", 15.0, 1));
+        cart.add(new VegItem("Garlic Bread", 5.0, 1));
+
+        double grandTotal = 0;
+
+        System.out.println("--- Your Order Receipt ---");
+        for (FoodItem item : cart) {
+            System.out.println(item.getItemDetails());
+            
+            double itemTotal = item.calculateTotalPrice();
+            grandTotal += itemTotal;
+            
+            
+            if (item instanceof Discountable) {
+                System.out.println(" > " + ((Discountable) item).getDiscountDetails());
+            }
+            
+            System.out.println(" Subtotal: $" + itemTotal);
+            System.out.println("--------------------------");
+        }
+
+        System.out.println("TOTAL AMOUNT TO PAY: $" + grandTotal);
+    }
+}
