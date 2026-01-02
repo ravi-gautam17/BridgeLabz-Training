e93ee302d27b045958edd32adc569881a7564afe
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-02 15:06:22 +0530

    [Ravi]: Cat program added

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/animal/Cat.java
@@ -0,0 +1,13 @@
+package com.inheritance.animal;
+
+public class Cat extends Animal{
+	
+	public Cat(String name, int age) {
+		super(name, age);
+	}
+	
+	@Override
+	public void makeSound() {
+		System.out.println("Cat meows");
+	}
+}
