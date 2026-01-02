6a7939a276d297ec7365fe73f43ce102c9544cb5
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-03 00:14:32 +0530

    [Ravi]: Book program added

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/librarymanagementsystem/Book.java
@@ -0,0 +1,23 @@
+package com.encapsulation.librarymanagementsystem;
+
+public class Book extends LibraryItem implements Reservable{
+	
+	public Book(String id, String title, String author) {
+		super(id, title, author);
+	}
+	
+	@Override
+	public int getLoanDuration() {
+		return 21;
+	}
+	
+	@Override
+	public void reserveItem(String memberId) {
+		System.out.println("Book "+getTitle()+" reserved for "+ memberId);
+	}
+	
+	@Override
+	public boolean checkAvailability() {
+		return !isLoaned;
+	}
+}
