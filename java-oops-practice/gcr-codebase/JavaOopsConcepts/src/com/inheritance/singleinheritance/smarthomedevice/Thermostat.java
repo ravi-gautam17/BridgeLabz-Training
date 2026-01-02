cb8848542ea5b54f71c0ef661f2da8f7a83f737b
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-02 14:22:12 +0530

    [Ravi]: Thermostat program added

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/singleinheritance/smarthomedevice/Thermostat.java
@@ -0,0 +1,17 @@
+package com.inheritance.singleinheritance.smarthomedevice;
+
+public class Thermostat extends Device{
+	
+	private int temperatureSetting; // in celcius (like 24 degree C)
+	
+	public Thermostat(int deviceId, String status, int temperatureSetting) {
+		super(deviceId, status);
+		this.temperatureSetting = temperatureSetting;
+	}
+	
+	@Override
+	public void displayStatus() {
+		super.displayStatus();
+		System.out.println("Temprature: " + temperatureSetting);
+	}
+}
