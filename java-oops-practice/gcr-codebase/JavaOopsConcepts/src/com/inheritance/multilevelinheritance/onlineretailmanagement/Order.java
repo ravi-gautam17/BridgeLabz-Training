328a01018c5bfbdcbf7116ff2634a72c5121bf06
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-02 14:31:45 +0530

    [Ravi]: Order program added

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevelinheritance/onlineretailmanagement/Order.java
@@ -0,0 +1,16 @@
+package com.inheritance.multilevelinheritance.onlineretailmanagement;
+
+public class Order {
+	
+	private String orderId;
+	private String orderDate;
+	
+	public Order(String orderId, String orderDate) {
+		this.orderId = orderId;
+		this.orderDate = orderDate;
+	}
+	
+	public String getOrderStatus() {
+		return "Order Placed";
+	}
+}
