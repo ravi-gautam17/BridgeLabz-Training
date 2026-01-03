c20c8054c35f1c82fa7c9abce22dc97622d6651b
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-03 14:25:45 +0530

    [Ravi]: Business wallet program added

---
+++ java-oops-practice/scenario-based-codebase/scenarioBasedPractice/src/com/oops/ewalletapplication/BusinessWallet.java
@@ -0,0 +1,23 @@
+package com.oops.ewalletapplication;
+
+public class BusinessWallet extends Wallet implements Transferrable{
+	private double taxRate = 0.02; // 2% business tax
+
+    public BusinessWallet(double initialLoad, double bonus) {
+        super(initialLoad, bonus);
+        this.transferLimit = 50000.0; // Much higher limit
+    }
+
+    @Override
+    public void transferTo(User receiver, double amount) {
+        double totalDeduction = amount + (amount * taxRate); // Using Operators
+        if (getBalance() >= totalDeduction) {
+            this.updateBalance(-totalDeduction);
+            receiver.getWallet().updateBalance(amount);
+            this.addLog("Business Transfer: $" + amount + " (Tax: $" + (amount * taxRate) + ")");
+        }
+    }
+
+    @Override
+    public void processTransaction(double amount) { /* Implementation */ }
+}
