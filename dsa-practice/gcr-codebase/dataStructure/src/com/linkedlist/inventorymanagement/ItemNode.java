957a92d6249b8f3af21bf5ede40f0546bc57dbf0
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-03 16:55:20 +0530

    [Ravi]: Node program added

---
+++ dsa-practice/gcr-codebase/dataStructure/src/com/linkedlist/inventorymanagement/ItemNode.java
@@ -0,0 +1,17 @@
+package com.linkedlist.inventorymanagement;
+
+public class ItemNode {
+	 int itemId;
+	    String itemName;
+	    int quantity;
+	    double price;
+	    ItemNode next;
+
+	    ItemNode(int itemId, String itemName, int quantity, double price) {
+	        this.itemId = itemId;
+	        this.itemName = itemName;
+	        this.quantity = quantity;
+	        this.price = price;
+	        this.next = null;
+	    }
+}
