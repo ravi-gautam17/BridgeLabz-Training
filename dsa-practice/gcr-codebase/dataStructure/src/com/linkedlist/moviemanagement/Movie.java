0db510264209895f63a688e1db85059a03be02fa
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-07 09:42:06 +0530

    [Ravi]: Movie program added

---
+++ dsa-practice/gcr-codebase/dataStructure/src/com/linkedlist/moviemanagement/Movie.java
@@ -0,0 +1,19 @@
+package com.linkedlist.moviemanagement;
+
+public class Movie {
+	 Movie perv;
+	 String movieTitle;
+     String director;
+     String yor;
+     float rating;
+     Movie next;
+    
+     public Movie(String movieTitle, String director, String yor, float rating) {
+ 		this.movieTitle = movieTitle;
+ 		this.director = director;
+ 		this.yor = yor;
+ 		this.rating = rating;
+ 		this.perv = null;
+ 		this.next = null;
+ 	}
+}
