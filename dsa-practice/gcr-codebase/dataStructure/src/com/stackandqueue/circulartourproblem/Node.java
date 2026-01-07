9aafb7cee500945531423c88b3ec4f5daf409fce
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-07 12:52:34 +0530

    [Ravi]: Node program added

---
+++ dsa-practice/gcr-codebase/dataStructure/src/com/stackandqueue/circulartourproblem/Node.java
@@ -0,0 +1,11 @@
+package stacksandqueue.circulartourproblem;
+
+public class Node {
+	   public PetrolPump data;
+	   public  Node next;
+
+	   public Node(PetrolPump data) {
+	        this.data = data;
+	        this.next = null;
+	    }
+}
