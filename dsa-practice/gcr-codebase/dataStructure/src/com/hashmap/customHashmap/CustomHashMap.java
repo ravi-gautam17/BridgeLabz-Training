bdeb568dafa0a4398942bec3fb402e5aebdae916
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-07 13:11:41 +0530

    [Ravi]: Custom hashmap program added

---
+++ dsa-practice/gcr-codebase/dataStructure/src/com/hashmap/customHashmap/CustomHashMap.java
@@ -0,0 +1,59 @@
+package hashmap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CustomHashMap{
+	
+	private Node[] map;
+	
+	public CustomHashMap() {
+		map = new Node[1000];
+		for(int i=0; i<1000; i++) {
+			map[i] = new Node(-1, -1);
+		}
+	}
+	
+	public void put(int key, int value) {
+		int hash = hash(key);
+		Node cur = map[hash];
+		
+		while(cur.next != null) {
+			if(cur.next.key == key) {
+				cur.next.val = value;
+				return;
+			}
+			cur = cur.next;
+		}
+		cur.next = new Node(key, value);
+	}
+	
+	public int get(int key) {
+		int hash = hash(key);
+		Node cur = map[hash].next;
+		
+		while(cur!= null) {
+			if(cur.key==key)
+				return cur.val;
+			cur= cur.next;
+		}
+		return -1;
+		
+	}
+	
+	public void remove(int key) {
+		int hash = hash(key);
+		Node cur = map[hash];
+		
+		while(cur.next!=null) {
+			if(cur.next.key == key) {
+				cur.next = cur.next.next;
+				return;
+			}
+			cur = cur.next;
+		}
+		
+	}
+	private int hash(int key) {
+		return key%1000;
+	}
+}
