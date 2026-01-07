9f8f0c0e7cdfafd4760b67232984ba883c77c2da
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-07 11:28:58 +0530

    [Ravi]: Text state program added

---
+++ dsa-practice/gcr-codebase/dataStructure/src/com/linkedlist/undoredo/TextState.java
@@ -0,0 +1,13 @@
+package com.linkedlist.undoredo;
+
+public class TextState {
+	String content;
+    TextState prev;
+    TextState next;
+
+    TextState(String content) {
+        this.content = content;
+        this.prev = null;
+        this.next = null;
+    }
+}
