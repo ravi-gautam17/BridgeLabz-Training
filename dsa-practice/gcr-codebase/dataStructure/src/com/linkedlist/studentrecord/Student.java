3ea48f0c1319687123e22731c6e8cd335c9b0791
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-07 11:19:09 +0530

    [Ravi]: Student program added

---
+++ dsa-practice/gcr-codebase/dataStructure/src/com/linkedlist/studentrecord/Student.java
@@ -0,0 +1,18 @@
+package com.linkedlist.studentrecord;
+
+
+public class Student {
+     public String name;
+     public int rollno;
+     public int age;
+     public char grade;
+     public Student next;
+	  public Student(String name, int rollno, int age, char grade) {
+		this.name = name;
+		this.rollno = rollno;
+		this.age = age;
+		this.grade = grade;
+		this.next = null;
+	  }
+      
+}
