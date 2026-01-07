d02a4a44af895c35c1fb93e22c57e2c821df17d7
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-07 11:09:45 +0530

    [Ravi]: FriendNode program added

---
+++ dsa-practice/gcr-codebase/dataStructure/src/com/linkedlist/socialmediaconnection/FriendNode.java
@@ -0,0 +1,11 @@
+package com.linkedlist.socialmediaconnection;
+
+public class FriendNode {
+	int friendId;
+    FriendNode next;
+
+    FriendNode(int friendId) {
+        this.friendId = friendId;
+        this.next = null;
+    }
+}
