6889f38407844b2846b90e41086dd46791c5ef49
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-10 00:19:02 +0530

    [Ravi]: Non veg item program added

---
+++ java-oops-practice/scenario-based-codebase/scenarioBasedPractice/src/com/oops/day7/foodloop/NonVegItem.java
@@ -0,0 +1,10 @@
+package com.oops.day7.foodloop;
+
+public class NonVegItem extends FoodItem{
+
+	public NonVegItem(String name, double price, int stock) {
+		super(name,"Non veg", price, stock);
+		
+	}
+     
+}
