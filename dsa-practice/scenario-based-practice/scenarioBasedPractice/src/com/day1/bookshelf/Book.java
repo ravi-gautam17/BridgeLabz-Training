7d2593a0c690a3de39ad4487881f4b2744f63d66
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-12 00:56:15 +0530

    [Ravi]: Book program added

---
+++ dsa-practice/scenario-based-practice/scenarioBasedPractice/src/com/day1/bookshelf/Book.java
@@ -0,0 +1,14 @@
+package com.day1.bookshelf;
+
+public class Book {
+    int id;
+    String author;
+    String title;
+    public Book(int id,String title,String author) {
+    	this.id=id;
+    	this.title=title;
+    	this.author=author;
+    }
+    
+}
+
