64f38b52f7d982d0afa02adf471704338711efe4
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-12 15:49:36 +0530

    [Ravi]: Page program added

---
+++ dsa-practice/scenario-based-practice/scenarioBasedPractice/src/com/day1/browserbuddy/Page.java
@@ -0,0 +1,14 @@
+package com.day1.browserbuddy;
+
+public class Page {
+    String url;
+    Page prev;
+    Page next;
+    public Page(String url) {
+    	this.url=url;
+    	this.prev=null;
+    	this.next=null;
+    	
+    }
+   
+}
