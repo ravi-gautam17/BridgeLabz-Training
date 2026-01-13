7b250392e04d0d07a1aa36ab03c2aec19e14ae2f
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-13 15:39:18 +0530

    [Ravi]: Service program added

---
+++ dsa-practice/scenario-based-practice/scenarioBasedPractice/src/com/day2/traincompanion/Service.java
@@ -0,0 +1,10 @@
+package com.day2.traincompanion;
+
+public class Service {
+    String serviceName;
+    
+    public Service(String serviceName) {
+    	this.serviceName=serviceName;
+    }
+    
+}
