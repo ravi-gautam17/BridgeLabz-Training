55d876f04e5627e29c4cde0c8261dfa76869142d
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-15 15:12:00 +0530

    [Ravi]: Main program added

---
+++ java-oops-practice/scenario-based-codebase/scenarioBasedPractice/src/com/oops/day9/birdsanctuary/Main.java
@@ -0,0 +1,19 @@
+package com.oops.day9.birdsanctuary;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        BirdSanctuary sanctuary = new BirdSanctuary();
+
+        sanctuary.addBird(new Eagle("B1", "Aquila"));
+        sanctuary.addBird(new Duck("B2", "Donald"));
+        sanctuary.addBird(new Penguin("B3", "Pingu"));
+        sanctuary.addBird(new Kiwi("B4", "Kiko"));
+
+        sanctuary.displayAllBirds();
+        sanctuary.displayFlyingBirds();
+        sanctuary.displaySwimmingBirds();
+        sanctuary.sanctuaryReport();
+    }
+}
