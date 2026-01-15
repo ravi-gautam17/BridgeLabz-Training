c3767b0bb94fe818a2df493e50cb8f3dd65ebac4
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-15 15:08:00 +0530

    [Ravi]: Duck program added

---
+++ java-oops-practice/scenario-based-codebase/scenarioBasedPractice/src/com/oops/day9/birdsanctuary/Duck.java
@@ -0,0 +1,24 @@
+package com.oops.day9.birdsanctuary;
+
+
+public class Duck extends Bird implements Flyable, Swimmable {
+
+    public Duck(String id, String name) {
+        super(id, name, "Duck");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getName() + " flies short distances.");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println(getName() + " swims in the pond.");
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println(getId() + " | " + getName() + " | Duck | Flyable & Swimmable");
+    }
+}
