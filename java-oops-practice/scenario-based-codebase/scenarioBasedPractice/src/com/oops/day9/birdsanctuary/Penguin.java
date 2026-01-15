7cbacc0fa74aee6ce9f5bad6e0abdd566a0abb6f
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-15 15:13:04 +0530

    [Ravi]: Penguin program added

---
+++ java-oops-practice/scenario-based-codebase/scenarioBasedPractice/src/com/oops/day9/birdsanctuary/Penguin.java
@@ -0,0 +1,19 @@
+package com.oops.day9.birdsanctuary;
+
+
+public class Penguin extends Bird implements Swimmable {
+
+    public Penguin(String id, String name) {
+        super(id, name, "Penguin");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println(getName() + " swims swiftly underwater.");
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println(getId() + " | " + getName() + " | Penguin | Swimmable");
+    }
+}
