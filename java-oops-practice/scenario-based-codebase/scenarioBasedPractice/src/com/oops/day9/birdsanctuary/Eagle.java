ee5b3ab0de2aff6a2d58b27357d710473e02eeb9
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-15 15:09:30 +0530

    [Ravi]: Eagle program added

---
+++ java-oops-practice/scenario-based-codebase/scenarioBasedPractice/src/com/oops/day9/birdsanctuary/Eagle.java
@@ -0,0 +1,17 @@
+package com.oops.day9.birdsanctuary;
+public class Eagle extends Bird implements Flyable {
+
+    public Eagle(String id, String name) {
+        super(id, name, "Eagle");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getName() + " soars high in the sky.");
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println(getId() + " | " + getName() + " | Eagle | Flyable");
+    }
+}
