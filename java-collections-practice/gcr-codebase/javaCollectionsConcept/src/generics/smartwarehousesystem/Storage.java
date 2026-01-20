973fb808c529ccc9b3fe7e9ce2042512a3235961
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-20 13:18:06 +0530

    [Ravi]: Storage program added

---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcept/src/generics/smartwarehousesystem/Storage.java
@@ -0,0 +1,15 @@
+package generics.smartwarehousesystem;
+import java.util.*;
+
+public class Storage<T extends WarehouseItem>{
+	
+	private List<T> items = new ArrayList<>();
+	
+	public void addItem(T item) {
+		items.add(item);
+	}
+	
+	public List<T> getItems(){
+		return items;
+	}
+}
