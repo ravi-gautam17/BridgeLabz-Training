3bb07b97623b0e039ca20a9d06a627e6ff6461a7
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-20 12:53:44 +0530

    [Ravi]: Vegan meal program added

---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcept/src/generics/MealPlanGenerator/VeganMeal.java
@@ -0,0 +1,17 @@
+package generics.MealPlanGenerator;
+
+public class VeganMeal implements MealPlan{
+
+	@Override
+	public String getMealType() {
+		// TODO Auto-generated method stub
+		return "Vegan";
+	}
+
+	@Override
+	public int getCalories() {
+		// TODO Auto-generated method stub
+		return 1700;
+	}
+	
+}
