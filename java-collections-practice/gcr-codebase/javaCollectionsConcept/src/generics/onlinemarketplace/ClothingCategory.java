f1046f8024a5630ce1be06706603f70f87b4e678
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-20 13:13:13 +0530

    [Ravi]: Clothing category program added

---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcept/src/generics/onlinemarketplace/ClothingCategory.java
@@ -0,0 +1,11 @@
+package generics.onlinemarketplace;
+
+public class ClothingCategory implements Category{
+
+	@Override
+	public String getCategoryName() {
+		// TODO Auto-generated method stub
+		return "Clothing";
+	}
+
+}
