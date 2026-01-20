d493804e311d85aa447c35eb1623ed3c9494c0da
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-20 12:45:32 +0530

    [Ravi]: Meal program added

---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcept/src/generics/MealPlanGenerator/Meal.java
@@ -0,0 +1,19 @@
+package generics.MealPlanGenerator;
+
+public class Meal<T extends MealPlan> {
+	private T mealPlan;
+
+    public Meal(T mealPlan) {
+        this.mealPlan = mealPlan;
+    }
+
+    public T getMealPlan() {
+        return mealPlan;
+    }
+
+    @Override
+    public String toString() {
+        return "Meal Plan: " + mealPlan.getMealType() +
+               ", Calories: " + mealPlan.getCalories();
+    }
+}
