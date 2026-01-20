34421fcaed0a88150cd1e0c1ac53fc71110ad8cd
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-20 13:09:58 +0530

    [Ravi]: Book category program added

---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcept/src/generics/onlinemarketplace/BookCategory.java
@@ -0,0 +1,11 @@
+package generics.onlinemarketplace;
+
+public class BookCategory implements Category{
+
+	@Override
+	public String getCategoryName() {
+		// TODO Auto-generated method stub
+		return "Books";
+	}
+
+}
