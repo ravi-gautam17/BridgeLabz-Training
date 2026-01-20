22b53a0ff2e75aaf9a2e6d92b5e265b41afc9a19
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-20 13:14:58 +0530

    [Ravi]: Gadget category program added

---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcept/src/generics/onlinemarketplace/GadgetCategory.java
@@ -0,0 +1,11 @@
+package generics.onlinemarketplace;
+
+public class GadgetCategory implements Category{
+
+	@Override
+	public String getCategoryName() {
+		// TODO Auto-generated method stub
+		return "Gadgets";
+	}
+
+}
