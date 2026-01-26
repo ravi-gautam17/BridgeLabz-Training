a1c24b9dd4cba04eeb58ebc0738b19dbbd783e4b
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-26 18:14:07 +0530

    [Ravi]: Censor bad words program added

---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcept/src/regex/BasicRegexProblem/CensorBadWords.java
@@ -0,0 +1,20 @@
+package regex.BasicRegexProblem;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class CensorBadWords {
+
+    public static void main(String[] args) {
+
+        String text = "This is a damn bad example with some stupid words.";
+
+        String[] badWords = {"damn", "stupid", "ugly"};
+
+        for (String bad : badWords) {
+            text = text.replaceAll("\\b" + bad + "\\b", "****");
+        }
+
+        System.out.println(text);
+    }
+}
