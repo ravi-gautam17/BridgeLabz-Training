aae5837d6a13d21de9f8024fbdd5dea8ff69df69
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-26 18:16:29 +0530

    [Ravi]: Extract link program added

---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcept/src/regex/BasicRegexProblem/ExtractLink.java
@@ -0,0 +1,20 @@
+package regex.BasicRegexProblem;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class ExtractLink {
+	public static final String LINK_REGEX = "https?://[^\\s]+";
+	
+	public static void main(String[] args) {
+		String text = "Visit https://www.google.com and http://example.org for more info.";
+		
+		Pattern pattern = Pattern.compile(LINK_REGEX);
+		Matcher matcher = pattern.matcher(text);
+		
+		while(matcher.find()) {
+			System.out.print(matcher.group()+", ");
+		}
+	}
+}
