f7052588cec7bf808ed5c618f0cb31243e903ea1
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-26 15:59:35 +0530

    [Ravi]: Read file content program added

---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcept/src/ExceptionHandling/ReadFileContent.java
@@ -0,0 +1,22 @@
+package ExceptionHandling;
+
+import java.io.IOException;
+import java.io.*;
+
+public class ReadFileContent {
+	public static void main(String[] args) {
+		
+		try {
+		FileReader fr = new FileReader("data.txt");
+		
+		int ch;
+		while((ch = fr.read())!=-1) {
+			System.out.print((char)ch);
+		}
+		fr.close();
+		}catch(IOException e) {
+			System.out.println("File not found");
+		}
+		
+	}
+}
