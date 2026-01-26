3ae6e3cb1cfe801e87d33d7a80e854f5b1abc8f1
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-26 18:17:11 +0530

    [Ravi]: Hexa color code verify program added

---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcept/src/regex/BasicRegexProblem/HexaColorCodeVerify.java
@@ -0,0 +1,24 @@
+package regex.BasicRegexProblem;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class HexaColorCodeVerify {
+	public static final String HEX_COLOR_REGEX = "^[#][0-9A-Za-z]{6}";
+	
+	public static boolean isValidCode(String hexaCode) {
+		Pattern pattern = Pattern.compile(HEX_COLOR_REGEX);
+		
+		Matcher matcher = pattern.matcher(hexaCode);
+		return matcher.matches();
+	}
+	
+	public static void main(String[] args) {
+		String[] arr=  {"#FFA500",	"#ff4500", "#123","FFA500"};
+		
+		for(String i: arr) {
+			System.out.println("hexa code: "+i+" is: "+
+					(isValidCode(i)? "Valid" : "Invlaid"));
+		}
+	}
+}
