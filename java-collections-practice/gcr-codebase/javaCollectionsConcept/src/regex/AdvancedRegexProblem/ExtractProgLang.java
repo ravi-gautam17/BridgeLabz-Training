935f0bfbaf30513eb3610c3c5985d0deebebacce
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-26 18:32:20 +0530

    [Ravi]: Extract programming language added

---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcept/src/regex/AdvancedRegexProblem/ExtractProgLang.java
@@ -0,0 +1,18 @@
+package regex.AdvancedRegexProblem;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class ExtractProgLang {
+	public static final String LANG_REGEX= "\\b(Python|Java|JavaScript|Go|C\\+\\+|Ruby|Rust)\\b";
+	
+	public static void main(String[] args) {
+		String text = "I love Java, Python, and JavaScript, but I haven't tried Go yet.";
+		
+		Pattern pattern = Pattern.compile(LANG_REGEX);
+		Matcher matcher = pattern.matcher(text);
+		
+		while(matcher.find()) {
+			System.out.print(matcher.group()+", ");
+		}
+	}
+}
