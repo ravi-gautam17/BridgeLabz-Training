db60cf99e19a104d4ae99cf93f91212ea1709c53
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-27 00:17:03 +0530

    [Ravi]: Paremeterized test program added

---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcept/src/junit/BasicJunitTesting/perameterizedtest/CheckEven.java
@@ -0,0 +1,7 @@
+package junit.BasicJunitTesting.perameterizedtest;
+
+public class CheckEven {
+	public boolean isEven(int a) {
+		return a%2==0;
+	}
+}
---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcept/src/junit/BasicJunitTesting/perameterizedtest/TestEvenNumber.java
@@ -0,0 +1,17 @@
+package junit.BasicJunitTesting.perameterizedtest;
+
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestEvenNumber {
+	CheckEven utils = new CheckEven();
+	
+	@ParameterizedTest
+    @ValueSource(ints = {2, 4, 6, 7, 9})
+    public void testIsEven(int number) {
+        boolean expected = (number % 2 == 0);
+        assertEquals(expected, utils.isEven(number), "Failed for number: " + number);
+    }
+}
