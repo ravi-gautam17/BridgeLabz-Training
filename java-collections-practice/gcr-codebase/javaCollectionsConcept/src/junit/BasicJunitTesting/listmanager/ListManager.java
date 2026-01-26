b0de5357713c14f8b3958891a0a296867185b3ca
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-27 00:16:06 +0530

    [Ravi]: List managers program added

---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcept/src/junit/BasicJunitTesting/listmanager/ListManager.java
@@ -0,0 +1,16 @@
+package junit.BasicJunitTesting.listmanager;
+
+import java.util.*;
+
+public class ListManager {
+	public void addElement(List<Integer> list, int element) {
+		list.add(element);
+	}
+	
+	public void removeElement(List<Integer> list, int element) {
+		list.remove(Integer.valueOf(element));
+	}
+	public int getSize(List<Integer> list) {
+		return list.size();
+	}
+}
---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcept/src/junit/BasicJunitTesting/listmanager/TestList.java
@@ -0,0 +1,53 @@
+package junit.BasicJunitTesting.listmanager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+class TestList {
+		
+	ListManager manager;
+    List<Integer> list;
+
+    @BeforeEach
+    void setup() {
+        manager = new ListManager();
+        list = new ArrayList<>();
+    }
+    @Test
+    void testAddElement() {
+        manager.addElement(list, 10);
+        manager.addElement(list, 20);
+
+        assertEquals(2, manager.getSize(list));
+        assertTrue(list.contains(10));
+        assertTrue(list.contains(20));
+    }
+
+    @Test
+    void testRemoveElement() {
+        manager.addElement(list, 10);
+        manager.addElement(list, 20);
+
+        manager.removeElement(list, 10);
+
+        assertEquals(1, manager.getSize(list));
+        assertFalse(list.contains(10));
+    }
+
+    @Test
+    void testGetSize() {
+        assertEquals(0, manager.getSize(list));
+
+        manager.addElement(list, 5);
+        manager.addElement(list, 15);
+
+        assertEquals(2, manager.getSize(list));
+    }
+
+}
