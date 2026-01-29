445f9ec3c52a07d6d0c6b222b22648007aaf8b3b
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-29 23:50:51 +0530

    [Ravi]: Filter JSON program added

---
+++ java-iostreams-practice/gcr-codebase/iostreamsPractice/src/com/json/practiceProblem2/FilterJson.java
@@ -0,0 +1,31 @@
+package com.json.practiceProblem2;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FilterJson {
+    public static void main(String[] args) throws IOException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readTree(new File("students.json"));
+
+        if (rootNode.isArray()) {
+            for (JsonNode student : rootNode) {
+
+                int age = student.path("age").asInt();
+
+                if (age > 25) {
+                    String name = student.path("name").asText();
+                    String dept = student.path("department").asText();
+
+                    System.out.println(
+                        "Name: " + name + ", age: " + age + ", department: " + dept
+                    );
+                }
+            }
+        }
+    }
+}
