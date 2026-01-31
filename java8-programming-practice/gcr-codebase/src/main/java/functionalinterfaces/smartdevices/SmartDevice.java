3ffb91df7fd37e812c8b7ee51621a0985ed8b4e1
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-01-31 20:40:20 +0530

    [Ravi]: Smart devices program added

---
+++ java8-programming-practice/gcr-codebase/src/main/java/functionalinterfaces/smartdevices/SmartDevice.java
@@ -0,0 +1,10 @@
+package functionalinterfaces.smartdevices;
+public interface SmartDevice {
+
+    // Method to turn the device ON
+    void turnOn();
+
+    // Method to turn the device OFF
+    void turnOff();
+}
+
---
+++ java8-programming-practice/gcr-codebase/src/main/java/functionalinterfaces/smartdevices/TV.java
@@ -0,0 +1,14 @@
+package functionalinterfaces.smartdevices;
+
+public class TV implements SmartDevice {
+
+    @Override
+    public void turnOn() {
+        System.out.println("TV is turned ON");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("TV is turned OFF");
+    }
+}
