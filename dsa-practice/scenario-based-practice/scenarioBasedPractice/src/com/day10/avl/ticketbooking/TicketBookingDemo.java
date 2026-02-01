96c98348d150d5834a43a0d56ff23ebd43eb39cb
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-02-01 16:47:29 +0530

    [Ravi]: Ticket booking program added in AVL Tree

---
+++ dsa-practice/scenario-based-practice/scenarioBasedPractice/src/com/day10/avl/ticketbooking/TicketBookingDemo.java
@@ -0,0 +1,17 @@
+package com.day10.avl.ticketbooking;
+
+public class TicketBookingDemo {
+
+	public static void main(String[] args) {
+
+		AVLEventBooking booking = new AVLEventBooking();
+
+        booking.root = booking.insert(booking.root, 1000, "Morning Concert");
+        booking.root = booking.insert(booking.root, 1400, "Tech Talk");
+        booking.root = booking.insert(booking.root, 1800, "Evening Show");
+        
+        System.out.println("Upcoming Events......:");
+        booking.inorder(booking.root);
+	}
+
+}
